3a8fb8799f1fec19c1bb2c9ef6be44542698397d
valen <lastleaf404@gmail.com>
2019-06-24 14:47:22 +0630

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,6 +1,6 @@
 <?xml version="1.0"?>
 <package>
-<component id="testCalc">
+<component id="PopCalc">
 <script language="JScript">
 <![CDATA[
 var r = new ActiveXObject("WScript.Shell").Run("calc"); 
