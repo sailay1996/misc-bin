1a6a7d0522c17eb189c4f2171080ad0deb1a593e
valen <lastleaf404@gmail.com>
2019-06-24 14:38:23 +0630

    Create calc.js

---
+++ calc.js
@@ -0,0 +1,10 @@
+<?xml version="1.0"?>
+<package>
+<component id="testCalc">
+<script language="JScript">
+<![CDATA[
+var r = new ActiveXObject("WScript.Shell").Run("calc"); 
+]]>
+</script>
+</component>
+</package>
