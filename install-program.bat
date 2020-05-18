dc7c4bbec9f26ef0ae9230d467ba0ffcaa672c07
valen <lastleaf404@gmail.com>
2020-05-18 23:55:54 +0630

    Create install-program.bat

---
+++ install-program.bat
@@ -0,0 +1,1 @@
+choco install firefox vscode dotnetcore-sdk windbg ghidra vscode-csharp notepadplusplus.install python2 python3 hxd windows-sdk-10 vscode-cpptools cutter sysinternals 7zip.install git.install
